663aeb8c176dc36121fc4598880040f1c8c0b363
Zelnatal <elnatan.honorato@gmail.com>
2023-08-15 20:36:35 -0300

    fix adicionando espaços depois do caminho

--- configGit.sh
+++ configGit.sh
@@ -3,9 +3,9 @@ cp /mnt/c/Users/$UserWindows/.gitconfig .gitconfig
 
 if [ -f "/mnt/c/Program Files/Git/mingw64/bin/git-credential-manager.exe" ]; then
     git config --global credential.helper "/mnt/c/Program\ Files/Git/mingw64/bin/git-credential-manager.exe"
-elif [ -f "/mnt/c/Program Files/Git/mingw64/libexec/git-core/git-credential-manager.exe"]; then
+elif [ -f "/mnt/c/Program Files/Git/mingw64/libexec/git-core/git-credential-manager.exe" ]; then
     git config --global credential.helper "/mnt/c/Program\ Files/Git/mingw64/libexec/git-core/git-credential-manager.exe"
-elif [ -f "/mnt/c/Program Files/Git/mingw64/bin/git-credential-manager-core.exe"]; then
+elif [ -f "/mnt/c/Program Files/Git/mingw64/bin/git-credential-manager-core.exe" ]; then
     git config --global credential.helper "/mnt/c/Program\ Files/Git/mingw64/bin/git-credential-manager-core.exe"
 else
     echo "Não encontrado o git-credential-manager"
