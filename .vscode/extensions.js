2c1b481805cfd7224cc8f3c286579d9528f099ad
giovanegurgel <49182936+giovanegurgel@users.noreply.github.com>
2023-08-22 08:17:12 -0300

    Create extensions.js

---
+++ .vscode/extensions.js
@@ -0,0 +1,15 @@
+{
+    "recommendations": [
+        "ms-azuretools.vscode-docker",
+        "onecentlin.laravel-extension-pack",
+        "pkief.material-icon-theme",
+        "christian-kohler.path-intellisense",
+        "devsense.phptools-vscode",
+        "ms-vscode-remote.vscode-remote-extensionpack",
+        "bradlc.vscode-tailwindcss",
+        "adrianwilczynski.alpine-js-intellisense",
+        "xabikos.javascriptsnippets",
+        "mgmcdermott.vscode-language-babel",
+        "bebo925.livewire-switcher"
+    ]
+}
